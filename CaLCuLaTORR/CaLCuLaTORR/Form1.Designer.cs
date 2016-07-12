5a8aeae680c0da78f884e3cda0da85587c6c875e
Hydrogen777 <sinceregordon18@gmail.com>
2016-07-13 00:13:03 +0600

    0.1.2(D)
    Ничего особого,просто накинул X и Y.

--- CaLCuLaTORR/CaLCuLaTORR/Form1.Designer.cs
+++ CaLCuLaTORR/CaLCuLaTORR/Form1.Designer.cs
@@ -47,18 +47,20 @@
             this.Cotangens = new System.Windows.Forms.Button();
             this.Factorial = new System.Windows.Forms.Button();
             this.Radical3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Location = new System.Drawing.Point(51, 51);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(105, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(288, 52);
+            this.textBox2.Location = new System.Drawing.Point(288, 51);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(105, 20);
             this.textBox2.TabIndex = 1;
@@ -231,11 +233,31 @@
             this.Radical3.UseVisualStyleBackColor = true;
             this.Radical3.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Y";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(457, 322);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Radical3);
             this.Controls.Add(this.Factorial);
             this.Controls.Add(this.Cotangens);
@@ -288,6 +310,8 @@
         private System.Windows.Forms.Button Cotangens;
         private System.Windows.Forms.Button Factorial;
         private System.Windows.Forms.Button Radical3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
