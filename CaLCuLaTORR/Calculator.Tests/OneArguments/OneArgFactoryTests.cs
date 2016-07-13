229384f3cd1f64da9b98e752e1738e60fd29ea3d
roman98 <roman.kharchenko.98@mail.ru>
2016-07-13 10:31:57 +0600

    0.1.3
    Tests for Factorys

---
+++ CaLCuLaTORR/Calculator.Tests/OneArguments/OneArgFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Calculator.OneArguments;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArguments
+{
+    [TestFixture]
+    public class OneArgFactoryTests
+    {
+        [TestCase("Cosinus", typeof(Cosinus))]
+        [TestCase("Factorial", typeof(Factorial))]
+        [TestCase("Sinus", typeof(Sinus))]
+        [TestCase("Cotangens", typeof(Cotangens))]
+        [TestCase("Inverseof", typeof(InverseOf))]
+        [TestCase("Lg", typeof(Lg))]
+        [TestCase("Ln", typeof(Ln))]
+        [TestCase("Radical", typeof(Radical))]
+        [TestCase("Radical3", typeof(Radical3))]
+        [TestCase("Tangens", typeof(Tangens))]
+        public void TwoArgFactoryTest(string name, Type type)
+        {
+            var calculator = OneArgFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
---
+++ CaLCuLaTORR/Calculator.Tests/TwoArguments/TwoArgFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calculator.TwoArguments;
+using NUnit.Framework;
+
+namespace Calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    public class TwoArgFactoryTests
+    {
+        [TestCase("plus", typeof(Plus))]
+        [TestCase("Mod", typeof(Mod))]
+        [TestCase("delenie", typeof(Division))]
+        [TestCase("umnojenie", typeof(Multiplication))]
+        [TestCase("Involution", typeof(Involution))]
+        [TestCase("minus", typeof(Minus))]
+        public void TwoArgFactoryTest(string name, Type type)
+        {
+            var calculator = TwoArgFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
